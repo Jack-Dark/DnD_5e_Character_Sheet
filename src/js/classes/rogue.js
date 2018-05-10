9d4116ce4056a763523c6993b75d5cac4d8ae9d7
Jack-Dark <decker3d@gmail.com>
2018-05-09 23:23:52 -0600

    Removes redundant class reward info.

--- src/js/classes/rogue.js
+++ src/js/classes/rogue.js
@@ -8,9 +8,8 @@ characterClass.rogue = {
 			{
 				title: 'Expertise',
 				description: [
-					'Choose two of your skill proficiencies, or one of your skill proficiencies and your proficiency with thieves’ tools. Your proficiency bonus is doubled for any ability check you make that uses either of the chosen proficiencies.',
-					'At 6th level, you can choose two more of your proficiencies (in skills or with thieves’ tools) to gain this benefit.'
-					]
+					'Choose two of your skill proficiencies, or one of your skill proficiencies and your proficiency with thieves’ tools. Your proficiency bonus is doubled for any ability check you make that uses either of the chosen proficiencies.'
+				]
 			},
 			{
 				title: 'Sneak Attack',
