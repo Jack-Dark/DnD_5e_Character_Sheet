8bc681b6ac10f0892ac0bef6740deccd8f7a41fa
Jack-Dark <decker3d@gmail.com>
2018-05-10 19:25:44 -0600

    Adds basic subclass json.

--- src/js/classes/rogue.js
+++ src/js/classes/rogue.js
@@ -3,15 +3,26 @@ characterClass.rogue = {
 		stat: 'dexterity',
 		stat: 'intelligence'
 	},
+	subclasses: [
+		'Arcane Trickster',
+		'Assassin',
+		'Inquisitive',
+		'Mastermind',
+		'Scout',
+		'Swashbuckler',
+		'Thief'
+	],
 	levelRewards: {
 		'_1': [
 			{
+				subclassAbility:false,
 				title: 'Expertise',
 				description: [
 					'Choose two of your skill proficiencies, or one of your skill proficiencies and your proficiency with thieves’ tools. Your proficiency bonus is doubled for any ability check you make that uses either of the chosen proficiencies.'
 				]
 			},
 			{
+				subclassAbility:false,
 				title: 'Sneak Attack',
 				description: [
 					'You know how to strike subtly and exploit a foe’s distraction. Once per turn, you can deal an extra 1d6 damage to one creature you hit with an attack if you have advantage on the attack roll. The attack must use a finesse or a ranged weapon.',
@@ -20,6 +31,7 @@ characterClass.rogue = {
 				]
 			},
 			{
+				subclassAbility:false,
 				title: 'Thieves’ Cant',
 				description: [
 					'During your rogue training you learned thieves’ cant, a secret mix of dialect, jargon, and code that allows you to hide messages in seemingly normal conversation. Only another creature that knows thieves’ cant understands such messages. It takes four times longer to convey such a message than it does to speak the same idea plainly.',
@@ -29,6 +41,7 @@ characterClass.rogue = {
 		],
 		'_2': [
 			{
+				subclassAbility:false,
 				title: 'Cunning Action',
 				description: [
 					'Your quick thinking and agility allow you to move and act quickly. You can take a bonus action on each of your turns in combat. This action can be used only to take the Dash, Disengage, or Hide action.'
@@ -37,14 +50,24 @@ characterClass.rogue = {
 		],
 		'_3': [
 			{
+				subclassAbility:false,
 				title: 'Roguish Archetype',
 				description: [
 					'You choose an archetype that you emulate in the exercise of your rogue abilities: Thief, Assassin, or Arcane Trickster, all detailed at the end of the class description. Your archetype choice grants you features at 3rd level and then again at 9th, 13th, and 17th level.'
+				]
+			},
+			{
+				subclassAbility:true,
+				subclassName: 'Assassin',
+				title: 'Assassinate',
+				description: [
+					'You are at your deadliest when you get the drop on your enemies. You have advantage on attack rolls against any creature that hasn’t taken a turn in the combat yet. In addition, any hit you score against a creature that is surprised is a critical hit.'
 				]
 			}
 		],
 		'_4': [
 			{
+				subclassAbility:false,
 				title: 'Ability Score Improvement',
 				description: [
 					'You can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature. Alternatively, you can instead choose a character feat.'
@@ -53,6 +76,7 @@ characterClass.rogue = {
 		],
 		'_5': [
 			{
+				subclassAbility:false,
 				title: 'Uncanny Dodge',
 				description: [
 					'When an attacker that you can see hits you with an attack, you can use your reaction to halve the attack’s damage against you.'
@@ -61,6 +85,7 @@ characterClass.rogue = {
 		],
 		'_6': [
 			{
+				subclassAbility:false,
 				title: 'Expertise',
 				description: [
 					'Choose two of your skill proficiencies, or one of your skill proficiencies and your proficiency with thieves’ tools. Your proficiency bonus is doubled for any ability check you make that uses either of the chosen proficiencies.'
@@ -69,6 +94,7 @@ characterClass.rogue = {
 		],
 		'_7': [
 			{
+				subclassAbility:false,
 				title: 'Evasion',
 				description: [
 					'You can nimbly dodge out of the way of certain area effects, such as a red dragon’s fiery breath or an ice storm spell. When you are subjected to an effect that allows you to make a Dexterity saving throw to take only half damage, you instead take no damage if you succeed on the saving throw, and only half damage if you fail.'
@@ -77,14 +103,27 @@ characterClass.rogue = {
 		],
 		'_8': [
 			{
+				subclassAbility:false,
 				title: 'Ability Score Improvement',
 				description: [
 					'You can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature. Alternatively, you can instead choose a character feat.'
 				]
 			}
 		],
+		'_9': [
+			{
+				subclassAbility:true,
+				subclassName: 'Assassin',
+				title: 'Infiltration Expertise',
+				description: [
+					'You can unfailingly create false identities for yourself. You must spend seven days and 25 gp to establish the history, profession, and affiliations for an identity. You can’t establish an identity that belongs to someone else. For example, you might acquire appropriate clothing, letters of introduction, and official-looking certification to establish yourself as a member of a trading house from a remote city so you can insinuate yourself into the company of other wealthy merchants.',
+					'Thereafter, if you adopt the new identity as a disguise, other creatures believe you to be that person until given an obvious reason not to.'
+				]
+			}
+		],
 		'_10': [
 			{
+				subclassAbility:false,
 				title: 'Ability Score Improvement',
 				description: [
 					'You can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature. Alternatively, you can instead choose a character feat.'
@@ -93,6 +132,7 @@ characterClass.rogue = {
 		],
 		'_11': [
 			{
+				subclassAbility:false,
 				title: 'Reliable Talent',
 				description: [
 					'You have refined your chosen skills until they approach perfection. Whenever you make an ability check that lets you add your proficiency bonus, you can treat a d20 roll of 9 or lower as a 10.'
@@ -101,14 +141,27 @@ characterClass.rogue = {
 		],
 		'_12': [
 			{
+				subclassAbility:false,
 				title: 'Ability Score Improvement',
 				description: [
 					'You can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature. Alternatively, you can instead choose a character feat.'
 				]
 			}
 		],
+		'_13': [
+			{
+				subclassAbility:true,
+				subclassName: 'Assassin',
+				title: 'Impostor',
+				description: [
+					'You gain the ability to unerringly mimic another person’s speech, writing, and behavior. You must spend at least three hours studying these three components of the person’s behavior, listening to speech, examining handwriting, and observing mannerisms.',
+					'Your ruse is indiscernible to the casual observer. If a wary creature suspects something is amiss, you have advantage on any Charisma (Deception) check you make to avoid detection.'
+				]
+			}
+		],
 		'_14': [
 			{
+				subclassAbility:false,
 				title: 'Blindsense',
 				description: [
 					'If you are able to hear, you are aware of the location of any hidden or invisible creature within 10 feet of you.'
@@ -117,6 +170,7 @@ characterClass.rogue = {
 		],
 		'_15': [
 			{
+				subclassAbility:false,
 				title: 'Slippery Mind',
 				description: [
 					'You have acquired greater mental strength. You gain proficiency in Wisdom saving throws.'
@@ -125,14 +179,26 @@ characterClass.rogue = {
 		],
 		'_16': [
 			{
+				subclassAbility:false,
 				title: 'Ability Score Improvement',
 				description: [
 					'You can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature. Alternatively, you can instead choose a character feat.'
 				]
 			}
 		],
+		'_17': [
+			{
+				subclassAbility:true,
+				subclassName: 'Assassin',
+				title: 'Death Strike',
+				description: [
+					'You become a master of instant death. When you attack and hit a creature that is surprised, it must make a Constitution saving throw (DC 8 + your Dexterity modifier + your proficiency bonus). On a failed save, double the damage of your attack against the creature.'
+				]
+			}
+		],
 		'_18': [
 			{
+				subclassAbility:false,
 				title: 'Elusive',
 				description: [
 					'You are so evasive that attackers rarely gain the upper hand against you. No attack roll has advantage against you while you aren’t incapacitated.'
@@ -141,6 +207,7 @@ characterClass.rogue = {
 		],
 		'_19': [
 			{
+				subclassAbility:false,
 				title: 'Ability Score Improvement',
 				description: [
 					'You can increase one ability score of your choice by 2, or you can increase two ability scores of your choice by 1. As normal, you can’t increase an ability score above 20 using this feature. Alternatively, you can instead choose a character feat.'
@@ -149,6 +216,7 @@ characterClass.rogue = {
 		],
 		'_20': [
 			{
+				subclassAbility:false,
 				title: 'Stroke of Luck',
 				description: [
 					'You have an uncanny knack for succeeding when you need to. If your attack misses a target within range, you can turn the miss into a hit. Alternatively, if you fail an ability check, you can treat the d20 roll as a 20. Once you use this feature, you can’t use it again until you finish a short or long rest.'
